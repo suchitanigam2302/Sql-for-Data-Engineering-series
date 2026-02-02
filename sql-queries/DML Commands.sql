6fed327ebc1ef1da632aac243a72539926c0f83e
suchitanigam <suchitanigam2302@gmail.com>
2026-02-02 18:59:59 +0530

    DML commands

--- sql-queries/DML Commands.sql
+++ sql-queries/DML Commands.sql
@@ -32,6 +32,68 @@ WHERE id = 6;
 SELECT *
 FROM customers
 
+/*CHANGE THE SCORE OF CUSTOMER 8 TO 0
+AND UPDATE THE COUNTRY TO UK*/
+
+UPDATE customers
+SET score = 0,
+	country = 'UK'
+WHERE id = 8;
+
+SELECT *
+FROM customers
+
+/*UPDATE ALL CUSTOMERS WITH A NULL SCORE BY SETTING THEIR SCORE TO 0 */
+
+UPDATE customers
+SET country = 'India'
+WHERE country IS NULL;
+
+SELECT *
+FROM customers
+
+
+/*CHANGE THE SCORE OF CUSTOMER 8 TO 0
+AND UPDATE THE COUNTRY TO UK*/
+
+UPDATE customers
+SET score = 0,
+	country = 'UK'
+WHERE id = 8;
+
+SELECT *
+FROM customers
+
+/*UPDATE ALL CUSTOMERS WITH A NULL SCORE BY SETTING THEIR SCORE TO 0 */
+
+UPDATE customers
+SET country = 'India'
+WHERE country IS NULL;
+
+SELECT *
+FROM customers
+
+/*CHANGE THE SCORE OF CUSTOMER 8 TO 0
+AND UPDATE THE COUNTRY TO UK*/
+
+UPDATE customers
+SET score = 0,
+	country = 'UK'
+WHERE id = 8;
+
+SELECT *
+FROM customers
+
+/*UPDATE ALL CUSTOMERS WITH A NULL COUNTRY BY SETTING THEIR COUNTRY TO India */
+
+UPDATE customers
+SET country = 'India'
+WHERE country IS NULL;
+
+SELECT *
+FROM customers
+
+
 --DELETE ALL CUSTOMERS WITH ID GREATER THAN 5
 
 DELETE FROM customers
@@ -39,7 +101,9 @@ WHERE id > 5;
 
 SELECT *
 FROM customers
+WHERE id > 5;
 
 --DELETE ALL DATA FROM PERSONS TABLE
 
 TRUNCATE TABLE persons;
+
