9954daede8a04751df9d9c0babf31836877a111f
suchitanigam <suchitanigam2302@gmail.com>
2026-02-02 18:21:40 +0530

    Insert Command Using Select

--- sql-queries/DML Commands.sql
+++ sql-queries/DML Commands.sql
@@ -1,8 +1,19 @@
 --DML Commands
---INSERT Command
+--INSERT Command MANUALLY
 
 INSERT INTO customers (id,first_name,country,score)
 VALUES 
 	(6,'Suchita','India',100),
 	(7,'Ankita',NULL,90),
 	(8,'Harsh','USA',NULL);
+
+--INSERT USING SELECT
+--INSERT DATA FROM CUSTOMERS TO PERSONS
+
+INSERT INTO persons(id,name,birth_date,email)
+SELECT
+id,
+first_name,
+NULL,
+'UNKNOWN'
+FROM customers;
