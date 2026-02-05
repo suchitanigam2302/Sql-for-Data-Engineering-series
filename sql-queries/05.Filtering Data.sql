a77497ee7497e840869fede7f889c66e6bed51a4
suchitanigam <suchitanigam2302@gmail.com>
2026-02-05 20:08:55 +0530

    Logical Operator(NOT)

--- sql-queries/05.Filtering Data.sql
+++ sql-queries/05.Filtering Data.sql
@@ -52,3 +52,8 @@ WHERE country = 'USA' AND score > 500;
 SELECT *
 FROM customers
 WHERE country = 'USA' OR score > 500;
+
+--RETRIVE ALL CUSTOMERS WITH A SCORE NOT LESS THAN 500
+SELECT *
+FROM customers
+WHERE NOT score < 500;
